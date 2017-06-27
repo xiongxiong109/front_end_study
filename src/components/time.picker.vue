7d99d117b7143bf65edd7064d5b2be849b372c6f
xjq熊剑桥 <jqxiong@Ctrip.com>
2017-06-27 10:29:47 +0800

    update picker

---
+++ src/components/time.picker.vue
@@ -0,0 +1,18 @@
+<!-- 3d轮盘选择器 -->
+<template>
+	<div class="time-picker">
+		<h4>3d轮盘选择器</h4>
+		<div class="picker-stage">
+			<div class="picker-container">
+				<div class="picker-item">1</div>
+				<div class="picker-item">2</div>
+				<div class="picker-item">3</div>
+			</div>
+		</div>
+	</div>
+</template>
+<script type="text/javascript">
+	export default {
+		name: 'time-picker'
+	}
+</script>
--- src/router/CSStudy/pages/d3_trans.vue
+++ src/router/CSStudy/pages/d3_trans.vue
@@ -3,37 +3,40 @@
 		<h4>3d变换</h4>
 		<p>translate-3d, perspective, transform-style, backface-visibility</p>
 		<br>
-		<p>实例练习: 轮盘指示picker</p>
+		<p>实例练习: 旋转木马 <button @click="isPlay = !isPlay">{{isPlay ? '暂停': '播放'}}</button></p>
 		<br>
 		<div class="test-stage">
 			<div class="test-container">
 				<!-- 使用负的animation-delay, 可以让动画瞬间出现在指定位置 -->
 				<div class="test-item" v-for="(item, idx) in list" :style="{
-					'animation-delay': (-idx / list.length * 3 * 2) + 's'
+					'animation-delay': (-idx / list.length * 3 * 2) + 's',
+					'animation-play-state': isPlay ? 'running': 'paused'
 				}"></div>
 			</div>
 		</div>
-		<!-- 舞台设置景深(视距) -->
-		<div class="picker-container">
-			<!-- 容器设置3d视图 -->
-<!-- 			<ul class="picker-scroll">
-				<li v-for="(item, idx) in list" :style="{
-					transform: 'rotateX('+(-idx * 360 / list.length)+'deg) translateY('+d3Y+'px)'
-				}">{{item}}</li>
-			</ul> -->
-		</div>
+		<p>一般一个3d的视图，需要一个舞台，一个容器，以及一些内容</p>
+		<br>
+		<p>舞台一般设置perspective景深, 来控制视距, 视距越大, 离得越远, 一般视距可设为容器元素的1.5倍</p>
+		<br>
+		<p>容器一般要设置transform-style: preserve-3d</p>
+		<p>然后就可以对元素进行3d变换了</p>
+		<time-picker />
 	</div>
 </template>
 <script type="text/javascript">
+	import TimePicker from '@/components/time.picker';
+
 	export default {
 		data() {
 			return {
+				isPlay: false,
 				list: []
 			}
 		},
 		computed: {
 			d3Y() {
 				let { list } = this;
+				// 用三角函数计算离Z轴的距离
 				let Y = 25 / Math.tan((360 / list.length));
 				return Y;
 			}
@@ -42,6 +45,9 @@
 			for (let i = 0; i < 10; i++) {
 				this.list.push(i);
 			}
+		},
+		components: {
+			[TimePicker.name]: TimePicker
 		}
 	}
 </script>
@@ -51,6 +57,7 @@
 }
 .test-container {
 	transform-style: preserve-3d;
+	transform: rotateY(15deg);
 	position: relative;
 	height: 50px;
 }
@@ -64,6 +71,7 @@
 	right: 0;
 	/*transform: rotateY(10deg) translateZ(50px);*/
 	animation: rotate 3s linear infinite;
+	animation-play-state: paused;
 }
 @keyframes rotate {
 	from {
