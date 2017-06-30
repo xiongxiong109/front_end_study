decb6e322794da151001fe9d8b6119f170f0b86c
xjq熊剑桥 <jqxiong@Ctrip.com>
2017-06-30 14:56:26 +0800

    圆环进度条

--- src/router/CSStudy/pages/cir_pro.vue
+++ src/router/CSStudy/pages/cir_pro.vue
@@ -1,18 +1,39 @@
 <template>
 	<div class="com-page">
 		<h4>圆形进度条</h4>
+		<input type="range" v-model="cur" :max="max">
+		<span>{{rad}}deg</span>
 		<div class="container-box">
 			<div class="progress">
 				<div class="left-circle">
-					<div class="half-circle"></div>
+					<div class="half-circle" :style="{
+						transform: `rotateZ(${rad > 180 ? rad - 180 : 0}deg)`
+					}"></div>
 				</div>
 				<div class="right-circle">
-					<div class="half-circle"></div>
+					<div class="half-circle" :style="{
+						transform: `rotateZ(${Math.min(180, rad)}deg)`
+					}"></div>
 				</div>
 			</div>
 		</div>
 	</div>
 </template>
+<script type="text/javascript">
+	export default {
+		data: () => {
+			return {
+				cur: 68,
+				max: 100
+			}
+		},
+		computed: {
+			rad() {
+				return Math.ceil(this.cur / this.max * 360);
+			}
+		}
+	}
+</script>
 <style type="text/css">
 	.container-box {
 		height: 300px;
@@ -29,13 +50,22 @@
 		align-items: center;
 		justify-content: center;
 		overflow: hidden;
+		border: 2px solid #fff;
+		background: #000;
+	}
+	.progress:after {
+		content: '';
+		position: absolute;
+		width: 146px;
+		height: 146px;
+		border-radius: 50%;
+		background-color: #fff;
 	}
 	.left-circle, .right-circle {
-		flex: 1;
-		height: 100%;
+		width: 150px;
+		height: 150px;
 		overflow: hidden;
 		position: relative;
-		background-color: #000;
 	}
 	.half-circle {
 		position: absolute;
@@ -43,7 +73,7 @@
 		left: 0;
 		width: 100%;
 		height: 100%;
-		background-color: #cdefcd;
+		background-color: #cdcdcd;
 	}
 	.left-circle .half-circle {
 		content: '';
